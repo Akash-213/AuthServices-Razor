3bd4f97ca50fd4a62343e3e46e7860acf2bd7b70
Akash-213 <akash213kulkarni@gmail.com>
2023-03-31 19:13:04 +0530

    Add project files.

---
+++ AuthServices/Areas/Identity/Data/AuthServicesContext.cs
@@ -0,0 +1,23 @@
+﻿using AuthServices.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthServices.Data;
+
+public class AuthServicesContext : IdentityDbContext<AuthServicesUser>
+{
+    public AuthServicesContext(DbContextOptions<AuthServicesContext> options)
+        : base(options)
+    {
+
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
